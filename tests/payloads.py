9ab13cbc2481432af115ce5586a48341d0119798
Nikita Bulavinov <ns.bulavinov@gmail.com>
2024-01-30 16:20:37 +0300

    Add payloads for testing

---
+++ tests/payloads.py
@@ -0,0 +1,83 @@
+from typing import Any
+from pydantic import BaseModel, SkipValidation, Field
+
+
+class Payload(BaseModel):
+    class Config:
+        extra = 'allow'
+        arbitrary_types_allowed = True
+
+
+class AircraftPayload(Payload):
+    orig_id: SkipValidation[Any] = Field(default=152, alias='id')
+    name: SkipValidation[Any] = 'Airbus 333'
+
+
+class CountryPayload(Payload):
+    name: SkipValidation[Any] = 'Russia'
+    region: SkipValidation[Any] = 'DOMESTIC'
+
+
+class AirportPayload(Payload):
+    orig_id: SkipValidation[Any] = Field(default=439, alias='id')
+    iata: SkipValidation[Any] = 'VRA'
+    icao: SkipValidation[Any] = 'MUVR'
+    code_ru: SkipValidation[Any] = 'ВРА'
+    name: SkipValidation[Any] = 'Хуан\xa0Гуальберто\xa0Гомес'
+    name_ru: SkipValidation[Any] = 'Хуан\xa0Гуальберто\xa0Гомес'
+    country: SkipValidation[Any] = CountryPayload()
+    city_ru: SkipValidation[Any] = 'Варадеро'
+    city_en: SkipValidation[Any] = 'Varadero'
+    lat: SkipValidation[Any] = 23.039896
+    long: SkipValidation[Any] = -81.436943
+    timezone: SkipValidation[Any] = 'America/Havana'
+
+
+class CompanyPayload(Payload):
+    name: SkipValidation[Any] = "Nordwind"
+    iata: SkipValidation[Any] = Field(default='N4', alias='code')
+    url_buy: SkipValidation[Any] = 'https://nordwindairlines.ru/'
+    url_register: SkipValidation[Any] = 'https://airbook.nordwindairlines.ru/check-in/?lang=ru#search'
+
+
+class FlightPayload(Payload):
+    orig_id: SkipValidation[Any] = Field(default=8770878, alias='id')
+    direction: SkipValidation[Any] = 'arrival'
+    number: SkipValidation[Any] = 556
+    date: SkipValidation[Any] = '2023-12-19T00:00:00+03:00'
+    mar1: SkipValidation[Any] = AirportPayload()
+    takeoff_et: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    at_other_et: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    mar2: SkipValidation[Any] = AirportPayload()
+    mar3: SkipValidation[Any] = None
+    mar4: SkipValidation[Any] = None
+    mar5: SkipValidation[Any] = None
+    at_local: SkipValidation[Any] = '2023-12-20T04:27:02+03:00'
+    term_local: SkipValidation[Any] = 'C'
+    term_local_prev: SkipValidation[Any] = 'C'
+    sked_local: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    at_local_et: SkipValidation[Any] = '2023-12-20T04:54:00+03:00'
+    chin_id: SkipValidation[Any] = '312-315'
+    chin_start: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    chin_end: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    gate_id: SkipValidation[Any] = '1a'
+    gate_id_prev: SkipValidation[Any] = '12'
+    boarding_start: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    boarding_end: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    bbel_id: SkipValidation[Any] = '1-2'
+    bbel_id_prev: SkipValidation[Any] = '1-2'
+    bbel_start: SkipValidation[Any] = '2023-12-20T04:56:00+03:00'
+    bbel_end: SkipValidation[Any] = '2023-12-20T05:25:00+03:00'
+    main_id: SkipValidation[Any] = 8770879
+    status_code: SkipValidation[Any] = 12
+    status_id: SkipValidation[Any] = 70
+    aircraft: SkipValidation[Any] = dict(id=152, name='aircraft name')
+    chin_start_et: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    chin_end_et: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    bbel_start_et: SkipValidation[Any] = '2023-12-20T04:56:00+03:00'
+    sked_other: SkipValidation[Any] = '2023-12-19T05:00:00+03:00'
+    at_other: SkipValidation[Any] = '2023-12-19T16:00:00+03:00'
+    way_time: SkipValidation[Any] = 693
+    otpr: SkipValidation[Any] = '2023-12-19T18:55:00+03:00'
+    prb: SkipValidation[Any] = None
+    company: SkipValidation[Any] = CompanyPayload()
--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -7,115 +7,27 @@ from src.svo_log_api.schemas import (
     FlightSchema,
 )
 
+from . import payloads
+
 
 class TestSchemas(TestCase):
 
     def test_valid_aircraft_model(self):
-        valid_aircraft = dict(id=152, name='aircraft name')
+        valid_aircraft = payloads.AircraftPayload().model_dump(by_alias=True)
         self.assertDictEqual(AircraftSchema(**valid_aircraft).model_dump(by_alias=True), valid_aircraft)
 
     def test_valid_country_model(self):
-        valid_country = dict(name='Russia', region='DOMESTIC')
+        valid_country = payloads.CountryPayload().model_dump(by_alias=True)
         self.assertDictEqual(CountrySchema(**valid_country).model_dump(by_alias=True), valid_country)
 
     def test_valid_airport_model(self):
-        valid_airport = dict(
-            id=439,
-            iata='VRA',
-            icao='MUVR',
-            code_ru='ВРА',
-            name='Хуан\xa0Гуальберто\xa0Гомес',
-            name_ru='Хуан\xa0Гуальберто\xa0Гомес',
-            country=dict(name='Russia', region='DOMESTIC'),
-            city_ru='Варадеро',
-            city_en='Varadero',
-            lat=23.039896,
-            long=-81.436943,
-            timezone='America/Havana',
-        )
+        valid_airport = payloads.AirportPayload().model_dump(by_alias=True)
         self.assertDictEqual(AirportSchema(**valid_airport).model_dump(by_alias=True), valid_airport)
 
     def test_valid_company_model(self):
-        valid_company = dict(
-            name='Nordwind Airlines',
-            code='N4',
-            url_buy='https://nordwindairlines.ru/',
-            url_register='https://airbook.nordwindairlines.ru/check-in/?lang=ru#search',
-        )
+        valid_company = payloads.CompanyPayload(code='ala').model_dump(by_alias=True)
         self.assertDictEqual(CompanySchema(**valid_company).model_dump(by_alias=True), valid_company)
 
     def test_valid_flight_model(self):
-        valid_flight = dict(
-            id=8770878,
-            direction='arrival',
-            number=556,
-            date='2023-12-19T00:00:00+03:00',
-            mar1=dict(
-                id=439,
-                iata='VRA',
-                icao='MUVR',
-                code_ru='ВРА',
-                name='Хуан\xa0Гуальберто\xa0Гомес',
-                name_ru='Хуан\xa0Гуальберто\xa0Гомес',
-                country=dict(name='Russia', region='DOMESTIC'),
-                city_ru='Варадеро',
-                city_en='Varadero',
-                lat=23.039896,
-                long=-81.436943,
-                timezone='America/Havana',
-            ),
-            takeoff_et='2023-12-19T18:55:00+03:00',
-            at_other_et='2023-12-19T18:55:00+03:00',
-            mar2=dict(
-                id=439,
-                iata='VRA',
-                icao='MUVR',
-                code_ru='ВРА',
-                name='Хуан\xa0Гуальберто\xa0Гомес',
-                name_ru='Хуан\xa0Гуальберто\xa0Гомес',
-                country=dict(name='Russia', region='DOMESTIC'),
-                city_ru='Варадеро',
-                city_en='Varadero',
-                lat=23.039896,
-                long=-81.436943,
-                timezone='America/Havana',
-            ),
-            mar3=None,
-            mar4=None,
-            mar5=None,
-            at_local='2023-12-20T04:27:02+03:00',
-            term_local='C',
-            term_local_prev='C',
-            sked_local='2023-12-19T18:55:00+03:00',
-            at_local_et='2023-12-20T04:54:00+03:00',
-            chin_id='312-315',
-            chin_start='2023-12-19T18:55:00+03:00',
-            chin_end='2023-12-19T18:55:00+03:00',
-            gate_id='1a',
-            gate_id_prev='12',
-            boarding_start='2023-12-19T18:55:00+03:00',
-            boarding_end='2023-12-19T18:55:00+03:00',
-            bbel_id='1-2',
-            bbel_id_prev='1-2',
-            bbel_start='2023-12-20T04:56:00+03:00',
-            bbel_end='2023-12-20T05:25:00+03:00',
-            main_id=8770879,
-            status_code=12,
-            status_id=70,
-            aircraft=dict(id=152, name='aircraft name'),
-            chin_start_et='2023-12-19T18:55:00+03:00',
-            chin_end_et='2023-12-19T18:55:00+03:00',
-            bbel_start_et='2023-12-20T04:56:00+03:00',
-            sked_other='2023-12-19T05:00:00+03:00',
-            at_other='2023-12-19T16:00:00+03:00',
-            way_time=693,
-            otpr='2023-12-19T18:55:00+03:00',
-            prb=None,
-            company=dict(
-                name='Nordwind Airlines',
-                code='N4',
-                url_buy='https://nordwindairlines.ru/',
-                url_register='https://airbook.nordwindairlines.ru/check-in/?lang=ru#search',
-            )
-        )
+        valid_flight = payloads.FlightPayload().model_dump(by_alias=True)
         self.assertDictEqual(FlightSchema(**valid_flight).model_dump(by_alias=True), valid_flight)
