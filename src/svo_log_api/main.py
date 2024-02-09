f8307ac68cdea7682fb78e0c39e78c0d86216ae0
Nikita Bulavinov <ns.bulavinov@gmail.com>
2024-02-09 13:26:41 +0300

    Update router endpoint

--- src/svo_log_api/main.py
+++ src/svo_log_api/main.py
@@ -3,6 +3,7 @@ from sqlalchemy_utils import create_database, database_exists
 
 from .database import engine, Base
 from . import models
+from .routers.airport_router import airport_router
 
 
 if not database_exists(url=engine.url):
@@ -12,3 +13,4 @@ Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(airport_router, prefix='/api/v1')
