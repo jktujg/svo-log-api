2e48162ce56e6fe9906e16ce0a3e6e536ffc3c7f
Nikita Bulavinov <ns.bulavinov@gmail.com>
2024-02-09 13:25:58 +0300

    Move directory field enum

--- src/svo_log_api/field_types.py
+++ src/svo_log_api/field_types.py
@@ -1,3 +1,4 @@
+from enum import StrEnum
 from typing import Annotated
 from datetime import datetime
 from sqlalchemy import text
@@ -6,3 +7,8 @@ from sqlalchemy.orm import mapped_column
 
 created_at = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
 updated_at = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=datetime.utcnow)]
+
+
+class Direction(StrEnum):
+    arrival = 'arrival'
+    departure = 'departure'
